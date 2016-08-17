65240e5e60cb3d297a5742488cb480c78b8b88b8
Bost <thebost@gmail.com>
2016-08-18 00:59:00 +0200

    cleanup 8

--- src/clj/quil_site/views/api.clj
+++ src/clj/quil_site/views/api.clj
@@ -1,9 +1,5 @@
 (ns quil-site.views.api
-  (:require [quil-site.views.page :refer [page]]
-            [hiccup.page :as p]
-            [hiccup.element :as e]
-            [hiccup.util :as u]
-            [clojure.string :as string]))
+  (:require [clojure.string :as string]))
 
 (defn as-url [str]
   (-> str
