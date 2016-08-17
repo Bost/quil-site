3f6af3a3abe9f7333068f51153c008614c43b7db
Bost <thebost@gmail.com>
2016-08-18 00:27:06 +0200

    cleanup 2

--- src/clj/quil_site/views/examples.clj
+++ src/clj/quil_site/views/examples.clj
@@ -6,20 +6,4 @@
   (page {:tab :examples
          :type :examples-page
          :js-files ["/js/main.js"]
-         :title "Quil Examples"}
-        [:div.row.examples
-         [:div#template {:class "col-md-4 col-sm-6 col-xs-12 hidden"}
-          [:div.example
-           [:div.name]
-           [:div.author]
-           [:div.canvas-container
-            [:canvas]
-            [:div.play.hidden]]
-           [:div.footer
-            [:span.glyphicon.glyphicon-sort.hidden
-             {:data-toggle "tooltip"
-              :data-placement "bottom"
-              :title (str "Interactive. Select to start interacting using "
-                          "mouse/keyboard.")}]
-            [:a "source"]
-            [:span.glyphicon.glyphicon-pause.hidden.pause]]]]]))
+         :title "Quil Examples"}))
