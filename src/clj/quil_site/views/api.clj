b0b41fea7c8ce430f26973ea94a2df8f09f17727
Bost <thebost@gmail.com>
2016-08-18 00:31:03 +0200

    cleanup 4

--- src/clj/quil_site/views/api.clj
+++ src/clj/quil_site/views/api.clj
@@ -5,14 +5,6 @@
             [hiccup.util :as u]
             [clojure.string :as string]))
 
-(def ^:private index-page-columns
-  [["Color" "Typography"]
-   ["Image" "Transform" "Rendering" ]
-   ["Math" "Data" "State"]
-   ["Shape"]
-   ["Lights, Camera" "Environment"]
-   ["Input" "Output" "Structure" "Middleware"]])
-
 (defn as-url [str]
   (-> str
       (string/lower-case)
@@ -75,77 +67,14 @@
          :title "Quil API"
          :js-files ["/js/api.js"]}))
 
-(defn- trim-docstring
-  "Removes extra spaces in the begginning of dostringing lines."
-  [dostringing]
-  (->> (string/split dostringing #"\n")
-       (map #(string/replace % #"^  " ""))
-       (string/join "\n")))
-
-(defn- render-function [fn]
-  (let [{:keys [name args subcategory docstring link type what
-                processing-name requires-bindings category ns]} fn
-        args (map #(if (vector? %) {:value % :type :both} %)
-                  args)
-        fields {:arguments
-                (for [{:keys [value type]} args]
-                  (render-type-specific :span.arg [:code (pr-str value)]
-                                         type what))
-
-                :docstring
-                [:pre.docstring (trim-docstring docstring)]
-
-                :binding?
-                (if requires-bindings "Yes" "No")
-
-                :original-name
-                (if processing-name
-                  (if link
-                    [:span (e/link-to link processing-name)]
-                    [:span processing-name])
-                  "None. It is present only in Quil.")}]
-    [:div.function-doc {:id (as-url name)}
-     (render-type-specific :h3 (str
-                                (if ns (str ns "/") "")
-                                name) type what)
-     [:div.row
-      [:div.col-md-7.col-xs-12
-       [:dl
-        [:dt "Arguments"] [:dd (:arguments fields)]
-        [:dt "Docstring"] [:dd (:docstring fields)]]]
-      [:div.col-md-5.col-xs-12
-       [:dl
-        [:dt "Works only inside sketch functions?"] [:dd (:binding? fields)]
-        [:dt "Original Processing method"] [:dd (:original-name fields)]]]]]))
-
-(defn- render-function-index [{:keys [name type what]}]
-  (render-type-specific :p.function (link name nil name)
-                        type what))
-
 (defn api-category [cat subcats]
   (page {:type :api
          :tab :api
          :title (str "Quil API " cat)
-         :js-files ["/js/api.js"]}
-        [:ol.breadcrumb
-         [:li (e/link-to "/api" "Index")]
-         [:li.active cat]]
-        (for [subcat (keys subcats)
-              :when subcat]
-          [:h4.subcategory (link subcat (str cat "/" subcat))])
-        [:div.function-index
-         (map render-function-index (subcats nil))]
-        (map render-function (subcats nil))))
+         :js-files ["/js/api.js"]}))
 
 (defn api-subcategory [cat subcat fns]
   (page {:type :api
          :tab :api
          :title (str "Quil API " subcat)
-         :js-files ["/js/api.js"]}
-        [:ol.breadcrumb
-         [:li (e/link-to "/api" "Index")]
-         [:li (link cat)]
-         [:li.active subcat]]
-        [:div.function-index
-         (map render-function-index fns)]
-        (map render-function fns)))
+         :js-files ["/js/api.js"]}))
