19fe69bf64b622b7a8dde677260add0b213e3a17
Bost <thebost@gmail.com>
2016-08-18 01:09:57 +0200

    cleanup 10

--- src/cljs/main/quil_site/main.cljs
+++ src/cljs/main/quil_site/main.cljs
@@ -15,117 +15,20 @@
   ([selector]
      (query-selector js/document selector)))
 
-(defn query-selector-all
-  ([element selector]
-     (.querySelectorAll element selector))
-  ([selector]
-     (query-selector-all js/document selector)))
-
 (extend-type js/NodeList
   ISeqable
   (-seq [array] (array-seq array 0)))
 
 (def examples (atom {}))
 
-(defn example-name->link [name]
-  (->> (cstr/replace name " " "-")
-       (str "/sketches/show/example_")))
-
-(defn pause-after-next-frame [sketch]
-  (let [real-sketch (aget (aget sketch "externals") "sketch")
-        set-on-frame-end #(aset real-sketch "onFrameEnd" %)]
-    (set-on-frame-end
-     (fn []
-       (q/with-sketch sketch
-         (q/no-loop))
-       (set-on-frame-end (fn []))))))
-
-(defn should-start-paused? []
-  (boolean (query-selector ".container.examples-page")))
-
-(defn setup-play-pause-functionality [host sketch]
-  (pause-after-next-frame sketch )
-  (let [play-button (query-selector host ".play")
-        pause-button (query-selector host ".pause")]
-    (classes/remove play-button "hidden")
-    (classes/remove pause-button "hidden")
-    (classes/add pause-button "invisible")
-    (events/listen play-button EventType/CLICK
-                   (fn []
-                     (q/with-sketch sketch
-                       (q/start-loop))
-                     (classes/add play-button "hidden")
-                     (classes/remove pause-button "invisible")))
-    (events/listen pause-button EventType/CLICK
-                   (fn []
-                     (pause-after-next-frame sketch)
-                     (classes/remove play-button "hidden")
-                     (classes/add pause-button "invisible")))))
-
-(defn run-example [example host]
-  (let [{:keys [name author run-fn interactive?]} example
-        link (example-name->link name)]
-    (when interactive?
-      (classes/remove (query-selector host ".glyphicon.hidden")
-                      "hidden"))
-    (dom/setProperties (query-selector host "a")
-                       #js {"href" link})
-    (dom/setTextContent (query-selector host ".name")
-                        name)
-    (dom/setTextContent (query-selector host ".author")
-                        (str "by " author))
-    (let [sketch (run-fn (query-selector host "canvas") 200)]
-      (when (should-start-paused?)
-        (setup-play-pause-functionality host sketch)))))
-
-(defn register-example! [name author run-fn &
-                         {:keys [interactive?]
-                          :or {interactive? false}
-                          :as st}]
-  (let [example (assoc st
-                       :name name
-                       :author author
-                       :run-fn run-fn)]
-    (if-let [host (@examples name)]
-      (run-example example host)
-      (swap! examples assoc name example))))
-
-(defn enable-tooltips []
-  ; I'm lazy to include proper externs now so I will use strings
-  ; Please fix it if your eyes are bleeding.
-  (let [tooltips ((aget js/window "$")
-                  "[data-toggle='tooltip']")]
-    (.call (aget tooltips "tooltip")
-           tooltips)))
-
 (def available-examples [])
-
-(defn get-examples-to-show []
-  (let [url (goog.Uri. js/document.URL)
-        debug-examples (set (.getParameterValues url "example"))]
-    (if (empty? debug-examples)
-      (shuffle available-examples)
-      debug-examples)))
-
-(defn load-example [name host]
-  (swap! examples assoc name host)
-  (let [script (dom/createElement "script")
-        file-name (str (cstr/replace name " " "_") ".js")]
-    (dom/setProperties script
-                       #js {"src" (str "/js/examples/" file-name)
-                            "async" "async"})
-    (dom/append js/document.body script)))
 
 (defn init-examples-page []
   (doseq [example-name available-examples]
     (let [host (.cloneNode (query-selector "#template") true)]
       (classes/remove host "hidden")
       (.appendChild (query-selector ".examples") host)
-      (.removeAttribute host "id")
-      (if-let [example (@examples example-name)]
-        (run-example example host)
-        (load-example example-name host))))
-  (enable-tooltips))
+      (.removeAttribute host "id"))))
 
 (defn init []
   (cond (query-selector ".container.examples-page")
